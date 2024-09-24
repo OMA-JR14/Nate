9bace94161dfc8d909c600df57db3287a674d903
OMA-JR14 <130357669+OMA-JR14@users.noreply.github.com>
2024-09-24 16:39:03 +0100

    Add files via upload

---
+++ ROCK PAPER SCISSIRS/ROCK PAPER SCISSIRS/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random random = new Random();
+bool PlayAGain = true;
+string Player;
+string Computer;
+string Answer;
+
+while (PlayAGain == true)
+{
+    Player = "";
+    Computer = "";
+    Answer = "";
+
+    while (Player != " ROCK " && Player != " PAPER " && Player != " SCISSORS ")
+    {
+        Console.WriteLine("Enter ROCK PAPER OR SCISSORS ");
+        Player = Console.ReadLine();
+        Player = Player.ToUpper();
+    }
+
+    switch (random.Next(1, 4))
+    {
+        case 1:
+            Computer = "ROCK";
+            break;
+        case 2:
+            Computer = "PAPER";
+            break;
+        case 3:
+            Computer = "SCISSORS";
+            break;
+    }
+    Console.WriteLine("player: "  + Player );
+    Console.WriteLine(" Computer:" + Computer );
+
+    switch (Player)
+    {
+        case "ROCK":
+            if (Computer == "ROCK")
+            {
+                Console.WriteLine(" DRAW ");
+            }
+            else if (Computer == "PAPER")
+            {
+                Console.WriteLine("YOU LOSE ");
+            }
+            else
+            {
+                Console.WriteLine(" YOU WIN ");
+            }
+            break;
+        case "PAPER":
+            if (Computer == "PAPER")
+            {
+                Console.WriteLine(" DRAW ");
+            }
+            else if (Computer == "SCISSORS ")
+            {
+                Console.WriteLine("YOU LOSE ");
+            }
+            else
+            {
+                Console.WriteLine(" YOU WIN ");
+            }
+            break;
+        case "Scissors":
+            if (Computer == "ROCK")
+            {
+                Console.WriteLine(" YOU LOSE  ");
+            }
+            else if (Computer == "PAPER")
+            {
+                Console.WriteLine(" YOU WIN");
+            }
+            else
+            {
+                Console.WriteLine(" YOU DRAW ");
+            }
+            break;
+
+
+    }
+    Console.WriteLine(" Would you like to play again Y/N: ");
+    Answer = Console.ReadLine();
+    Answer = Answer.ToUpper();
+
+    if (Answer == "Y")
+    {
+        PlayAGain = true;
+    }
+    else
+    {
+        PlayAGain = false;
+    }
+
+
+}
+
+Console.WriteLine(" THANKS FOR PLAYING ");
